f52cf370cdbafe779cdce009a9c65318fa2939c0
Peter Hickman <peterhickman386@googlemail.com>
2016-03-04 08:53:40 +0000

    because the lack of a sleep function in lua drives me nuts

---
+++ sleep/msleep.c
@@ -0,0 +1,30 @@
+/* 
+ * gcc -shared -fPIC -o msleep.so -llua msleep.c
+ *
+ * http://www.troubleshooters.com/codecorn/lua/lua_lua_calls_c.htm
+ */
+
+#include <unistd.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+static int msleep_c(lua_State *L){
+	long msecs = lua_tointeger(L, -1);
+	usleep(1000*msecs);
+	return 0;
+}
+
+/* Can't name this sleep(), it conflicts with sleep() in unistd.h */
+static int sleep_c(lua_State *L){
+	long secs = lua_tointeger(L, -1);
+	sleep(secs);
+	return 0;
+}
+
+/* Register both functions */
+int luaopen_msleep(lua_State *L){
+	lua_register( L, "msleep", msleep_c);  
+	lua_register(L, "sleep", sleep_c);
+	return 0;
+}
