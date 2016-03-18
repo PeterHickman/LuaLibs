f000bde617bf85f2f3d1d9ddda6ccfd06c80b683
Peter Hickman <peterhickman386@googlemail.com>
2016-03-18 22:15:49 +0000

    minor whitespace

--- split_on_whitespace.lua
+++ split_on_whitespace.lua
@@ -10,46 +10,47 @@ end
 
 --[[
 function test(source, expected)
-  local x = split_on_whitespace(source);
+  local x = split_on_whitespace(source)
 
   if #x ~= #expected then
-    print("Not Ok. Not the same length. Expected " .. #expected .. ", actual " .. #x);
+    print("Not Ok. Not the same length. Expected " .. #expected .. ", actual " .. #x)
     return
   end
 
-  local ok = true;
-  local error_at = nil;
-  local error_e = nil;
-  local error_a = nil;
+  local ok = true
+  local error_at = nil
+  local error_e = nil
+  local error_a = nil
 
   for i, e in ipairs(x) do
     if e ~= expected[i] then
-      error_at = i;
-      error_e  = expected[i];
-      error_a  = e;
-      ok = false;
-      break;
+      error_at = i
+      error_e  = expected[i]
+      error_a  = e
+      ok = false
+      break
     end
   end
 
   if ok == true then
-    print("Ok");
+    print("Ok")
   else
-    print("Not Ok. Elements mismatch at [" .. error_at .. "] expected [" .. error_e .. "], actual [" .. error_a .. "]");
+    print("Not Ok. Elements mismatch at [" .. error_at .. "] expected [" .. error_e .. "], actual [" .. error_a .. "]")
   end
 end
 
-test("a", {"a"});
-test(" a ", {"a"});
-test("    a   ", {"a"});
+test("a", {"a"})
+test(" a ", {"a"})
+test("    a   ", {"a"})
 
-test("a b", {"a","b"});
-test("a   b", {"a","b"});
-test("  a b  ", {"a","b"});
+test("a b", {"a","b"})
+test("a   b", {"a","b"})
+test("  a b  ", {"a","b"})
 
 -- These tests will fail
-test("a b", {"a","x"});
-test("a b",{"ab"});
+
+test("a b", {"a","x"})
+test("a b",{"ab"})
 test("a b c", {"x","y","z"})
 
 --]]
