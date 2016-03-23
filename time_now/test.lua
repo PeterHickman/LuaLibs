99fb7ff86e613b9ecbb45266195bd8677256223a
Peter Hickman <peterhickman386@googlemail.com>
2016-03-23 11:03:23 +0000

    more tests

--- time_now/test.lua
+++ time_now/test.lua
@@ -1,5 +1,6 @@
 require("time_now")
 
+print("A simple test")
 t = {}
 
 for i = 1,10 do
@@ -8,4 +9,18 @@ end
 
 for k,v in pairs(t) do
 	print(k,v)
+end
+print("")
+
+print("Hammer the garbage collector")
+function build()
+	local t = {}
+	for i = 1,100000 do
+		t[i] = time_now()
+	end
+end
+
+for i = 1,10 do
+	print("Iteration " .. i)
+	build()
 end
