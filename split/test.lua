e66e4b2aa4b73580a065a6009492ec3c8798c4e3
Peter Hickman <peter@abelsoninfo.com>
2016-12-22 12:10:22 +0000

    slightly better tests

--- split/test.lua
+++ split/test.lua
@@ -2,6 +2,18 @@ require 'split'
 
 test_number = 0
 
+--
+-- The actual message will be something like:
+-- "./split.lua:14: The marker cannot be an empty string"
+-- however the prefix is OS specific and the tests will fail
+-- if a new line is added to the file. So we remove the prefix
+--
+
+function clean_error(text)
+  local s, e = text:find(": ")
+  return text:sub(e+1)
+end
+
 function test(s, m, r)
   local x = split(s, m)
   local ok = true
@@ -25,6 +37,22 @@ function test(s, m, r)
   end
 end
 
+function fail(s, m, e)
+  test_number = test_number + 1
+
+  local status, err = pcall(function () split(s,m) end)
+
+  err = clean_error(err)
+
+  if status then
+    print(test_number .. " Error not raised")
+  elseif err == e then
+    print(test_number .. " Ok")
+  else
+    print(test_number .. " wrong error thrown. Expected=[" .. e .. "], actual=[" .. err .. "]")
+  end
+end
+
 test("1x234x5556",'x', {'1', '234', '5556'})
 test("1", 'x', {'1'})
 test("1x2", 'x', {'1','2'})
@@ -42,3 +70,6 @@ test('1', '11', { '1' })
 test('1xxx2', 'xx', { '1', 'x2'})
 test('1xxxx2', 'xx', { '1', '', '2'})
 test('1xxxxx2', 'xx', { '1', '', 'x2'})
+fail('1,2,3','', 'The marker cannot be an empty string')
+fail('1,2,3', nil, 'The marker cannot be an empty string')
+test('1,2,3', 2, { '1,', ',3'})
