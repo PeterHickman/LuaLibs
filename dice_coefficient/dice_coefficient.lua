20cac281ec2d5599fb7144f466e78189e5deee3c
Peter Hickman <peter@abelsoninfo.com>
2017-01-18 10:38:53 +0000

    whitespace issues

--- dice_coefficient/dice_coefficient.lua
+++ dice_coefficient/dice_coefficient.lua
@@ -15,46 +15,46 @@ local function create_bigrams(string)
   --
   -- It does assume that the string is at least 2 characters long!
 
-	assert(string:len() >= 2, "String needs to be at least 2 characters")
+  assert(string:len() >= 2, "String needs to be at least 2 characters")
 
-	local t = {}
+  local t = {}
 
-	local bigram
-	local this_char = ''
-	local next_char = string:sub(1,1)
-	local total = 0
+  local bigram
+  local this_char = ''
+  local next_char = string:sub(1,1)
+  local total = 0
 
-	for i = 2,#string do
-		this_char = next_char
-		next_char = string:sub(i,i)
+  for i = 2,#string do
+    this_char = next_char
+    next_char = string:sub(i,i)
 
-		if this_char ~= ' ' and next_char ~= ' ' then
-			total = total + 1
-			bigram = this_char .. next_char
-			if t[bigram] then
-				t[bigram] = t[bigram] + 1
-			else
-				t[bigram] = 1
-			end
-		end
-	end
+    if this_char ~= ' ' and next_char ~= ' ' then
+      total = total + 1
+      bigram = this_char .. next_char
+      if t[bigram] then
+        t[bigram] = t[bigram] + 1
+      else
+        t[bigram] = 1
+      end
+    end
+  end
 
-	return t, total
+  return t, total
 end
 
 function dice_coefficient(s1, s2)
-	local s1b, s1t = create_bigrams(s1)
-	local s2b, s2t = create_bigrams(s2)
+  local s1b, s1t = create_bigrams(s1)
+  local s2b, s2t = create_bigrams(s2)
 
-	local total = s1t + s2t
-	local same = 0
+  local total = s1t + s2t
+  local same = 0
 
-	for k,v1 in pairs(s1b) do
-		v2 = s2b[k]
-		if v2 ~= nil then
-			same = same + (v1 < v2 and v1 or v2) * 2
-		end
-	end
+  for k,v1 in pairs(s1b) do
+    v2 = s2b[k]
+    if v2 ~= nil then
+      same = same + (v1 < v2 and v1 or v2) * 2
+    end
+  end
 
-	return same / total
+  return same / total
 end
--- dice_coefficient/test.lua
+++ dice_coefficient/test.lua
@@ -3,27 +3,27 @@ require 'dice_coefficient'
 test_number = 0
 
 function float_equal(lhs, rhs)
-	local abs = math.abs
-	local epsilon = 1E-12 * 0.5
-	return abs(lhs - rhs) <= epsilon * (abs(lhs) + abs(rhs))
+  local abs = math.abs
+  local epsilon = 1E-12 * 0.5
+  return abs(lhs - rhs) <= epsilon * (abs(lhs) + abs(rhs))
 end
 
 function test(s1, s2, e)
-	local a = dice_coefficient(s1, s2)
+  local a = dice_coefficient(s1, s2)
 
-	test_number = test_number + 1
+  test_number = test_number + 1
 
-	if float_equal(a, e) then
-		print(test_number .. " Ok")
-	else
-		print(test_number .. " Failed. Expected " .. e .. ", actual " .. a)
-	end
+  if float_equal(a, e) then
+    print(test_number .. " Ok")
+  else
+    print(test_number .. " Failed. Expected " .. e .. ", actual " .. a)
+  end
 end
 
 test("banana","banana", 1.0)
 test("cat", "cap", 0.5)
 test("xxx", "yyy", 0.0)
 test("banana", "BANANA", 0.0)
-test("banana", "bananana", 0.8333333333333)
+test("banana", "bananana", 0.83333333333333)
 test("aaa", "aaza", 0.4)
 test("aaza", "aaa", 0.4)
